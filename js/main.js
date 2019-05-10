61566a2316e47350d033f59c82a9883c81654264
ernandesferreira <ernandes.dev@gmail.com>
2019-05-10 15:51:05 -0300

    apagando o main teste

--- js/main.js
+++ js/main.js
@@ -1,12 +1,40 @@
 $(document).ready(function(){
-  var value = $('#input')
-  $('#submit').click(function(){    
-    var n = value.val();
-    var c = 0;
-    while (n > 1) {
-      n = n % 2 ? 3 * n + 1 : n / 2;
-      c++;
-    }
-    $("#output").text(c);
-  });  
+  var numberTest = 1000000;
+  var number = 0;
+  var size = 0;
+  var highestSequence = 1;
+  var positionHighestSequence = 1;
+  var i = 0;
+
+  $("#calc").prop("disabled", false);
+
+  $('#calc').click(function(){
+    for (i=1; i <= numberTest; i++){
+      number = i;
+      size = 1;
+      while(number!=1){
+          if(number%2 == 0){
+              number = number/2;
+              }else{
+              number = number*3 + 1;
+          }
+          size++;
+      }
+      if(size > highestSequence){
+          highestSequence = size;
+          positionHighestSequence = i;
+      }
+  }
+   console.log('highestSequence: ', highestSequence );
+   console.log('positionhighestSequence: ', positionHighestSequence );
+
+   $("#calc").prop("disabled", true);
+
+   $("#output").html("O número inteiro positivo abaixo de 1 milhão que produz a sequência com a maioria dos elementos é o número: <strong>"+positionHighestSequence+"</strong>, e o número de etapas é de <strong>"+highestSequence+"</strong> etapas.");
+  });
+
+ 
+ 
+    
+  
 });
